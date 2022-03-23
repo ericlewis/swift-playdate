6597a67f050bce09458aaf8afad28488c9f7a855
Eric Lewis <ericlewis777@gmail.com>
2022-03-23 18:23:29 -0400

    move files

--- Example/Sources/SwiftHelloWorld/SwiftHelloWorld.swift
+++ Example/Sources/SwiftHelloWorld/SwiftHelloWorld.swift
@@ -1,16 +1,14 @@
 import Playdate
 
-let pd = Playdate.shared
-
 func updateCallback() -> Bool {
 
   Graphics.clear(with: .white)
   System.drawFPS(x: 0, y: 0)
 
   if System.currentButtonState == [.left, .up] {
-    Graphics.draw(.text("Pressing up & left! \(System.currentTimeInterval)", x: 0, y: 20))
+    Graphics.draw(.text("Pressing up & left!", x: 0, y: 20))
   } else {
-    Graphics.draw(.text("Hello world! \(System.currentTimeInterval)", x: 0, y: 20))
+    Graphics.draw(.text("Time elapsed: \(System.currentTimeInterval)", x: 0, y: 20))
   }
 
   Graphics.drawFilledRectangle(.init(x: 40, y: 40, width: 60, height: 30), color: .xor)
@@ -25,34 +23,43 @@ enum Fonts: String, CaseIterable {
   case italic
 }
 
+func initialize() {
+  do {
+    try Graphics.setFont(AshevilleSans, weight: .bold, size: .pt14)
+    Display.isInverted = true
+    setupMenu()
+  } catch {
+    System.error(error.localizedDescription)
+  }
+}
+
+func setupMenu() {
+  try! Menu.addCheckmarkItem("inverted", isOn: true) { isEnabled in
+    Display.isInverted = isEnabled
+  }
+
+  try! Menu.addCheckmarkItem("crnk snd", isOn: true) { isEnabled in
+    System.isCrankSoundEnabled = isEnabled
+  }
+
+  try! Menu.addOptionItem("font", options: Fonts.allCases) { option in
+    switch Fonts(rawValue: option) {
+    case .bold:
+      try! Graphics.setFont("Asheville-Sans-14-Bold")
+    case .light:
+      try! Graphics.setFont("Asheville-Sans-14-Light")
+    case .italic:
+      try! Graphics.setFont("Asheville-Sans-14-Light-Oblique")
+    case .none:
+      break
+    }
+  }
+}
+
 @_dynamicReplacement(for: EventCallback(event:))
 func eventCallback(event: SystemEvent) {
   if event == .initialize {
-    Display.isInverted = true
-
-    try! Graphics.setFont(AshevilleSans, weight: .bold, size: .pt14)
-
-    try! Menu.addCheckmarkItem("inverted", isOn: true) { isEnabled in
-      Display.isInverted = isEnabled
-    }
-
-    try! Menu.addCheckmarkItem("crnk snd", isOn: true) { isEnabled in
-      System.isCrankSoundEnabled = isEnabled
-    }
-
-    try! Menu.addOptionItem("font", options: Fonts.allCases) { option in
-      switch Fonts(rawValue: option) {
-      case .bold:
-        try! Graphics.setFont("Asheville-Sans-14-Bold")
-      case .light:
-        try! Graphics.setFont("Asheville-Sans-14-Light")
-      case .italic:
-        try! Graphics.setFont("Asheville-Sans-14-Light-Oblique")
-      case .none:
-        break
-      }
-    }
-
+    initialize()
     System.setUpdateCallback(updateCallback)
   }
 }
--- Sources/Playdate/Playdate.swift
+++ Sources/Playdate/Playdate.swift
@@ -1,17 +1,16 @@
 import CPlaydate
 
-public class Playdate {
-  public static let shared = Playdate()
-
+class Playdate {
+  static let shared = Playdate()
+  
   var pd: PlaydateAPI!
   var system: playdate_sys!
-  public private(set) var graphics: playdate_graphics!
-  public private(set) var display: playdate_display!
+  var graphics: playdate_graphics!
+  var display: playdate_display!
 
-  internal var eventHandler: ((SystemEvent) -> Void)? = nil
-  internal var updateCallback: (() -> Bool)? = nil
+  var updateCallback: (() -> Bool)? = nil
 
-  public func register(_ pd: PlaydateAPI) {
+  func register(_ pd: PlaydateAPI) {
     self.pd = pd
     self.system = pd.system.pointee
     self.graphics = pd.graphics.pointee
