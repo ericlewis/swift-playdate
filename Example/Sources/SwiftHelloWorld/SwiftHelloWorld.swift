f8aa39104dfe37e3a14aee9f7ec5c8de9cdeecf7
Eric Lewis <ericlewis777@gmail.com>
2022-03-23 15:27:54 -0400

    Update SwiftHelloWorld.swift

--- Example/Sources/SwiftHelloWorld/SwiftHelloWorld.swift
+++ Example/Sources/SwiftHelloWorld/SwiftHelloWorld.swift
@@ -4,8 +4,9 @@ let pd = Playdate.shared
 
 func updateCallback() -> Bool {
 
+  pd.graphics.clear(with: .white)
   pd.system.drawFPS(x: 0, y: 0)
-  pd.graphics.draw(.text("Hello world. \(pd.system.batteryPercentage)", x: 0, y: 20))
+  pd.graphics.draw(.text("Hello world. \(pd.system.currentTimeInterval)", x: 0, y: 20))
 
   return true
 }
@@ -14,5 +15,12 @@ func updateCallback() -> Bool {
 func eventCallback(event: SystemEvent) {
   if event == .initialize {
     pd.system.setUpdateCallback(updateCallback)
+    let _ = try! Menu.shared.addCheckmarkItem("testing2", isOn: true) { isEnabled in
+      pd.system.log(isEnabled ? "on" : "off")
+    }
+
+    let _ = try! Menu.shared.addOptionItem("boo", options: ["test1", "test2", "test3"]) {
+      pd.system.log($0)
+    }
   }
 }
